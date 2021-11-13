dd9e091a843489950a611685df5bc93a24e68c85
A1esandr <a1exswan001@gmail.com>
2021-11-14 01:03:15 +0500

    changed tg lib

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module msglogger
 
 go 1.17
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-)
+require github.com/A1esandr/tgbotapi v0.2.0
--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import app2 "msglogger/app"
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/A1esandr/tgbotapi"
+)
+
+//go:embed token.txt
+var embedToken string
 
 func main() {
-	app := app2.NewApp()
-	app.Start()
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		token = embedToken
+	}
+	if token == "" {
+		log.Panic("token is empty!")
+	}
+	bot, err := tgbotapi.New(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	offset := 0
+	for {
+		resp, err := bot.GetUpdates(&tgbotapi.GetUpdates{
+			Offset:  offset,
+			Limit:   10,
+			Timeout: 1,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if resp != nil {
+			for _, upd := range resp.Result {
+				fmt.Println("Update ID", upd.UpdateID)
+				fmt.Println("Chat ID", upd.ChannelPost.Chat.ID)
+				fmt.Println("Chat", upd.ChannelPost.Chat.Title)
+			}
+			if len(resp.Result) < 10 {
+				break
+			}
+			offset += 10
+		} else {
+			break
+		}
+	}
+	fmt.Println("finish")
 }
